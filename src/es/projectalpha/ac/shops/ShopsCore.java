48e599e622ec4ce997c2a457eb5c93198ba766d8
Wikijito7 <wikyfg@gmail.com>
2016-07-05 11:17:48 +0200

    Shops

--- src/es/projectalpha/ac/shops/ShopsCore.java
+++ src/es/projectalpha/ac/shops/ShopsCore.java
@@ -10,6 +10,20 @@ public class ShopsCore {
 			return Shops.NEWS.getReward();
 		case "car":
 			return Shops.CAR.getReward();
+		case "pizza":
+			return Shops.PIZZA.getReward();
+		case "donut":
+			return Shops.DONUT.getReward();
+		case "ship":
+			return Shops.SHIP.getReward();
+		case "hockey":
+			return Shops.HOCKEY.getReward();
+		case "cinema":
+			return Shops.CINEMA.getReward();
+		case "bank":
+			return Shops.BANK.getReward();
+		case  "oil":
+			return Shops.OIL.getReward();
 		default:
 			return 0;
 		}
